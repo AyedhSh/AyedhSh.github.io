4d04e57561e199becb4a1a781bfb472b469c4bb8
AyedhSh <alshmary_ad@yahoo.com>
2017-04-01 17:45:12 +0300

    finshing adding Youtube calling

--- js/youtube.js
+++ js/youtube.js
@@ -3,14 +3,42 @@
 
 $(document).ready(function(){
 $('#button2').click(function(){
-  getVedio();
+  getVideo();
 });
 });
 
-function getVedio(){
-  $.get('GET https://www.googleapis.com/youtube/v3/channels',{
+function getVideo(){
+  $.get('https://www.googleapis.com/youtube/v3/channels',{
     part: 'contentDetails',
-    forUsername: 'LearnCode',
+    // forUsername: 'LearnCode',
+    // forUsername: 'TechGuyWeb',
+    forUsername: 'Googledevelopers',
+    key: 'AIzaSyALp-l1KjrmC39wjPUwapy8mSHqjPKtmok'},
+    
+    function(data){
+      $.each(data.items,function(key,value){
+        console.log(value);
+        pid=value.contentDetails.relatedPlaylists.uploads;
+        callVideies(pid);
+      });
+    }
+    );
+}
 
-  });
+function callVideies(pid){
+$.get('https://www.googleapis.com/youtube/v3/playlistItems',{
+    part: 'snippet',
+    maxresults: 10,
+    playlistId: pid,
+    key: 'AIzaSyALp-l1KjrmC39wjPUwapy8mSHqjPKtmok'},
+    
+    function(data){
+      $.each(data.items,function(key,value){
+        console.log(value);
+        videoId = value.snippet.resourceId.videoId;
+        $('#result').append('<li><iframe src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>');
+      });
+    }
+    );
+
 }
