5cd7b268590a5d853bbfadddf865e5301d5ee784
AyedhSh <alshmary_ad@yahoo.com>
2017-04-07 15:06:50 +0300

    Make all page responsive

--- js/about.js
+++ js/about.js
@@ -1,6 +1,15 @@
 $.getJSON( "skill.json", function( data ) {
   $.each( data, function( key, val ) {
-    $('#appendix').append( "<li id='" + key + "'>" + val + "</li>" );
+    $(`#${key}`).append( val);
   });
  
+});
+$(function(){
+$('.box').hover(function(){
+ $(this).width(20%);
+  $(this).height(20%);
+},function(){
+  $(this).width(10%);
+  $(this).height(10%);
+});
 });
