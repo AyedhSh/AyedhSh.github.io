39a3c3e024985c34f42fa9c2aac91c3604c15bae
AyedhSh <alshmary_ad@yahoo.com>
2017-04-12 13:28:58 +0300

    add table to books page

--- js/books.js
+++ js/books.js
@@ -2,8 +2,10 @@
 // secret: gtvbs3xmM1ArjB8qrA1fufxVOTz52O0AiGTDYSsBg
 // previewLink   ===> it's the link of book on google to watch
 // volumeInfo.smallThumbnail.thumbnail == small image book
-
-var whichBookYouWantToSearchFor = "Frankenstein";
+$('button').click(function(){
+  var whichBookYouWantToSearchFor = "Frankenstein";
+var whichBookYouWantToSearchFor2 = $('input').val();
+console.log(whichBookYouWantToSearchFor2);
 
 $.ajax({
   url: "https://www.googleapis.com/books/v1/volumes?q=" + whichBookYouWantToSearchFor + "&key=AIzaSyA4tOZ9uG-5f6G53bt9Z4SyUUbCcJSgfjQ",
@@ -16,6 +18,12 @@ $.ajax({
     // debugger;
     $('#book').append(`<img src=${response.items["0"].volumeInfo.imageLinks.thumbnail}>`);
     console.log(response);
+     console.log(response.authors);
     console.log(response.items[0].volumeInfo.description);
+    $('#table1').append('<tr><th>Book Title</th><th>Author Name</th> <th>Description</th></tr><tr><td>'+`${response.items[0].volumeInfo.title}`+'</td><td>'+`${response.items[0].volumeInfo.authors}`+'</td><td>'+`${response.items[0].volumeInfo.description}`+'</td></tr>');
   }
 });
+
+});
+
+// '<tr><th>Book Title</th><th>Author Name</th> <th>Description</th></tr><tr><td>+`${response.title}`+</td><td>+`${response.authors[0]}`+</td><td>+`${response.items[0].volumeInfo.description}`+</td></tr>'
